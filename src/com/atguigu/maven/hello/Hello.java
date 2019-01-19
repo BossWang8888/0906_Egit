5a45b90b502414a6ccc90ad04182c8c1208dda74
jack <jack@163.com>
2019-01-19 14:32:39 +0800

    update by commit third

--- src/com/atguigu/maven/hello/Hello.java
+++ src/com/atguigu/maven/hello/Hello.java
@@ -7,5 +7,7 @@ public static void main(String[] args) {
 	System.out.println("hello!");
 	System.out.println("hello!");
 	System.out.println("hello!");
-}
+	System.out.println("hello mmmmm!");
+	
+	}
 }
